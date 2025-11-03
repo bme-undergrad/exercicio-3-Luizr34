bafd8fbde476a758e2691ccdb0b9920036e35e8a
Luiz Roberto Brandão <luiz.roberto.ffs34@gmail.com>
2025-11-02 22:48:48 -0300

    Update comments in exercicio3.m for clarity

--- exercicio3.m
+++ exercicio3.m
@@ -1,5 +1,5 @@
-# Resolvendo a EDO de Primeira Ordem, obtemos a função X = Xo.e^(μ.t), onde Xo é o número de bactérias em t = 0 e μ a taxa de crescimento das bactérias.
-# => [ln] => ln(X) = ln(Xo) + μ.t. Seja ln(Xo) = ao, μ = a1, e yi = ln(X), temos que: yi = ao + a1.t. Resolvendo esta linearização, é possível encontrar μ.
+% Resolvendo a EDO de Primeira Ordem, obtemos a função X = Xo.e^(μ.t), onde Xo é o número de bactérias em t = 0 e μ a taxa de crescimento das bactérias.
+% => [ln] => ln(X) = ln(Xo) + μ.t. Seja ln(Xo) = ao, μ = a1, e yi = ln(X), temos que: yi = ao + a1.t. Resolvendo esta linearização, é possível encontrar μ.
 
 function [mu] = exercicio3(t, NC)
 
